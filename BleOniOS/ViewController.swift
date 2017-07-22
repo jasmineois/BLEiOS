f30652edfcffcbd4574b273deacced46dffda3c6
たけのこ <michaelbobu@gmail.com>
2017-07-22 14:10:07 +0900

    Initial Commit

---
+++ BleOniOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BleOniOS
+//
+//  Created by たけのこ on 2017/07/22.
+//  Copyright © 2017年 たけのこ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
